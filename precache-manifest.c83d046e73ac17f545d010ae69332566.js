bf6de78d760310b9942f039c9b0d12355bbf475c
Moises Montilla <cl_moises@hotmail.com>
2019-04-11 23:31:55 -0300

    Updates

---
+++ precache-manifest.c83d046e73ac17f545d010ae69332566.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c46c7d8645621e275cfb4bacdb6123e",
+    "url": "/static/media/loading.0c46c7d8.gif"
+  },
+  {
+    "revision": "002040282939a0d2a1e69014721aa5d0",
+    "url": "/static/media/dog_shelter.00204028.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "27e419b424278ffb7dee",
+    "url": "/static/js/main.27e419b4.chunk.js"
+  },
+  {
+    "revision": "afb789456584730eb050",
+    "url": "/static/js/2.afb78945.chunk.js"
+  },
+  {
+    "revision": "27e419b424278ffb7dee",
+    "url": "/static/css/main.dfaf6cc5.chunk.css"
+  },
+  {
+    "revision": "267660e02c32bf9fbd5f5f85732584db",
+    "url": "/index.html"
+  }
+];
